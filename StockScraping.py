c8c9f3e24ed353c85f7007d3879b58824ced2191
cheq326 <cheq326@hotmail.com>
2015-03-03 23:58:33 -0500

    First version

---
+++ StockScraping.py
@@ -0,0 +1,29 @@
+__author__ = 'Dennis'
+
+import mysql.connector
+import sys
+import urllib.request
+import re
+import ystockquote
+
+db = mysql.connector.connect(host="localhost", # your host, usually localhost
+                     user="chenq", # your username
+                      passwd="admin", # your password
+                      db="stock") # name of the data base
+
+cur = db.cursor()
+cur.execute("select symbol from stocksymbol")
+rows = cur.fetchall()
+for (symbol,) in rows:
+    print(ystockquote.get_price(symbol))
+print("test")
+#print(ystockquote.get_all('GOOG'))
+#print(ystockquote.get_historical_prices('GOOG', '2015-01-01', '2015-02-01'))
+
+data = ystockquote.get_historical_prices('GOOG', '2015-01-01', '2015-02-01')
+for (date, dailyData) in data.items():
+    print(date)
+    for (key, value) in dailyData.items():
+        print(key + " " + value)
+cur.close()
+db.close()
