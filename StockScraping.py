12fdeb266382acee00ab11e8a7fb4134683cd263
Dennis <cheq326@hotmail.com>
2015-03-08 00:16:32 -0500

    minor update

--- StockScraping.py
+++ StockScraping.py
@@ -5,25 +5,25 @@ import sys
 import urllib.request
 import re
 import ystockquote
+import datetime
 
 db = mysql.connector.connect(host="localhost", # your host, usually localhost
                      user="chenq", # your username
                       passwd="admin", # your password
-                      db="stock") # name of the data base
+                      db="test") # name of the data base
 
 cur = db.cursor()
-cur.execute("select symbol from stocksymbol")
+cur.execute("select symbol from stock_symbol")
 rows = cur.fetchall()
+start_date = (datetime.datetime.today() - datetime.timedelta(days=3)).strftime('%Y-%m-%d')
+print (start_date)
+end_date = datetime.datetime.today().strftime('%Y-%m-%d')
+print(end_date)
 for (symbol,) in rows:
-    print(ystockquote.get_price(symbol))
-
-#print(ystockquote.get_all('GOOG'))
-#print(ystockquote.get_historical_prices('GOOG', '2015-01-01', '2015-02-01'))
-
-data = ystockquote.get_historical_prices('GOOG', '2015-01-01', '2015-02-01')
-for (date, dailyData) in data.items():
-    print(date)
-    for (key, value) in dailyData.items():
-        print(key + " " + value)
+    data = ystockquote.get_historical_prices(symbol, start_date, end_date)
+    for (date, dailyData) in data.items():
+        print(date)
+        for (key, value) in dailyData.items():
+            print(symbol + " " + key + " " + value)
 cur.close()
 db.close()
